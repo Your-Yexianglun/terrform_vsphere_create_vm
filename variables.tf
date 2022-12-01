8c7814173bd0fd6b1b572ccc29a0e81eb6e18cd1
wh_pluto <83756864+Your-Yexianglun@users.noreply.github.com>
2022-12-01 17:01:08 +0800

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "vsphere_user" {
+  type = string
+  default = "administrator@vsphere.local"
+}
+
+variable "vsphere_password" {
+  type    = string
+  default = "P@ssw0rd"
+}
+
+variable "vsphere_server" {
+  type = string
+  default = "192.168.110.100"
+}
